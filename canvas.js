a52c4d2b6d39e64c6bde11367abee9e7887e52f5
shuyang <qwqw1337@gmail.com>
2017-05-13 01:58:03 -0400

    Canvas

--- canvas.js
+++
@@ -1,6 +0,0 @@
-var c = document.getElementById("Canvas");
-var draw = c.getContext("2d");
-
-draw.moveTo(0,0);
-draw.lineTo(200,100);
-draw.stroke();
---
+++ js/canvas.js
@@ -0,0 +1,6 @@
+var c = document.getElementById("Canvas");
+var draw = c.getContext("2d");
+
+draw.moveTo(0,0);
+draw.lineTo(200,100);
+draw.stroke();
