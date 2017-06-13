b6c731172b83dc5b600b9b6c637676c14f3f534a
Shuyang <qwqw1337@gmail.com>
2017-06-13 16:09:44 -0400

    Created mechanism to have a rectangle follow mouse x position

--- js/canvas.js
+++ js/canvas.js
@@ -1,9 +1,14 @@
 var c = document.getElementById("Canvas");
 var ctx = c.getContext("2d");
-ctx.strokeStyle = "White"
+ctx.strokeStyle = "#FFFFFF";
+var rectStartX = 400;
+
+init();
 
 function rect(){
-	ctx.rect(20,20,150,100);
+	//arg1 = startX, arg2 = startY
+	//arg3 = horizontal length, arg4 = vertical length
+	ctx.rect(rectStartX-35,860,70,20);	
 }
 
 function line(){
@@ -11,7 +16,52 @@ function line(){
 	ctx.lineTo(200,100);
 }
 
-rect();
-line();
+document.addEventListener('mousemove', function(event){
+	ctx.beginPath();
+	var pos = getMousePos(c, event);
+	if(pos.x > 55 && pos.x < 735){
+		rectStartX = pos.x
+	}
+	ctx.clearRect(0, 0, 800, 900);
+	ref();
+});
 
-ctx.stroke();
+// document.addEventListener('keydown', function(event){
+// 	ctx.beginPath();
+// 	if(event.keyCode == 65 /* a pressed */){
+// 		if(rectStartX > 20){
+// 			rectStartX -= 5;
+// 		}
+// 	}
+// 	else if(event.keyCode == 68 /* d pressed */){
+// 		if(rectStartX < 780){
+// 			rectStartX += 5;
+// 		}
+// 	}
+	
+// 	ctx.clearRect(0, 0, 800, 900);
+// 	ref();
+// });
+
+function ref(){
+	rect();
+	ctx.stroke();
+}
+
+function init(){
+	ref();
+}
+
+function loop(){
+	while(true){
+		ctx
+	}
+}
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+}
